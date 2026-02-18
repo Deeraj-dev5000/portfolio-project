1968050c22bf81e59812f2d5a8d58295afb4b4a3
Deeraj A <deeraja53@gmail.com>
2026-02-18 21:04:46 +0530

    env debug

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+console.log("ENV CHECK:", process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
